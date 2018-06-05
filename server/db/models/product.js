68c7663062287a981fd996d551367a9c6f38ccdd
johnnybee4e <jon.rosado42@gmail.com>
2018-06-05 15:45:57 -0400

    built product model, added updateInventory instance method

---
+++ server/db/models/product.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    validate: {
+      min: 0.00,
+      notEmpty: true
+    }
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue: 'https://image.freepik.com/free-icon/single-cube_318-36160.jpg'
+  },
+  category: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  rating: {
+    type: Sequelize.DECIMAL,
+    validate: {
+      min: 1,
+      max: 5
+    }
+  }
+})
+
+Product.prototype.updateInventory = function(num) {
+  this.inventory = this.inventory - num;
+}
+
+module.exports = Product;
---
+++ server/db/models/product.spec.js
@@ -0,0 +1,40 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const Product = db.model('product')
+
+describe('Product model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instanceMethods', () => {
+    describe('updateInventory', () => {
+      let item
+
+      beforeEach(() => {
+        return Product.create({
+          title: 'Boullion Cube',
+          description: 'chicken stock',
+          price: 4.99,
+          inventory: 5,
+          category: ['food'],
+          rating: 3.4
+        })
+          .then(product => {
+            item = product
+          })
+      })
+
+      it('returns true if the password is correct', () => {
+        item.updateInventory(2)
+        expect(item.inventory).to.be.equal(3)
+      })
+
+      // it('returns false if the password is incorrect', () => {
+      //   expect(item.correctPassword('bonez')).to.be.equal(false)
+      // })
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
