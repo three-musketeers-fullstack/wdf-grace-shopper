47f2d9073dd933a11fd8b817ccc382f656bfbcc1
Ozal Khan <ozalkhan196@gmail.com>
2018-06-05 15:46:20 -0400

    testing singleproduct#7

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import { SingleProduct } from './components';
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <SingleProduct />
       <Routes />
     </div>
   )
