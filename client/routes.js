2ea9f51d26037ef28a0fa7a51afa67202110d995
augusto-proano <tito4gsus@gmail.com>
2018-06-07 12:14:15 -0400

    creating front end routing for the cart component

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, Cart} from './components'
+import Homepage from './components/Homepage.jsx';
 import {me} from './store'
 
 /**
@@ -19,6 +20,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/homepage" component={Homepage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path='/cart' component={Cart} />
@@ -31,6 +33,7 @@ class Routes extends Component {
         }
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
+        <Route exact path="/homepage" component={Homepage} />
       </Switch>
     )
   }
