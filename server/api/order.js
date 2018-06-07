a1f8f3ceb1a898300bb172594265aef4a76052b4
Ozal Khan <ozalkhan196@gmail.com>
2018-06-07 15:41:04 -0400

    feat(API) order routes created for purchasing cart and find previous orders

---
+++ server/api/order.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const { Order, User } = require("../db/models");
+module.exports = router;
+
+router.get("/", (req, res, next) => {
+  Order.findAll()
+    .then(orders => res.json(orders))
+    .catch(next);
+});
+
+//TO change is purchased in cart
+router.put("/:userId", (req, res, next) => {
+  Order.update(
+    {
+      isPurchased: true
+    },
+    {
+      where: { userId: req.params.userId }
+    }
+  );
+});
+
+//To get all previous purchases for User
+
+router.put('/:userId', (req,res,next) => {
+    Order.findAll(
+        {
+            where: {userId: req.params.id}
+        }
+    )
+})
+
+// router.put('/:orderId',(req,res,next) => {
+//     Order
+// })
