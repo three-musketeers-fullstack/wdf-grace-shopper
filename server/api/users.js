7deaa8a6b154ffd427bbd9f927d6974b3830d616
Ozal Khan <ozalkhan196@gmail.com>
2018-06-08 17:23:21 -0400

    order-product association table made, getroute for cart created uses eagerloading

--- server/api/users.js
+++ server/api/users.js
@@ -1,14 +1,32 @@
-const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
+const router = require("express").Router();
+const { User, Order, OrderProduct, Product } = require("../db/models");
 
-router.get('/', (req, res, next) => {
+module.exports = router;
+
+router.get("/", (req, res, next) => {
   User.findAll({
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    attributes: ["id", "email"]
   })
     .then(users => res.json(users))
-    .catch(next)
-})
+    .catch(next);
+});
+
+router.get("/:userId", (req, res, next) => {
+  User.findById(req.params.userId, {
+    include: [
+      {
+        model: Order,
+        include: [
+          {
+            all: true
+          }
+        ]
+      }
+    ]
+  })
+    .then(user => res.send(user))
+    .catch(next);
+});
