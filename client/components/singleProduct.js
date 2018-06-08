3bca56c343f6bdd78a27d582575c5681f7599b57
Augusto Proano <tito4gsus@gmail.com>
2018-06-08 15:16:08 -0400

    (render): single product page is being render (#40)
    Single product page successfully filtering and rendering.

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,65 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchSingleProduct } from '../store';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchSingleProduct } from "../store";
 
 const SingleProduct = props => {
-  console.log(props, '>>>>>>>>>>>>>>');
-  console.log('<-------------------->');
-  if (!props.products) {
-    props.onLoad(Number(props.match.params.id));
-    console.log(props, 'post onLoad');
-    return <h1>No Products</h1>;
-  } else {
-    const {
-      name,
-      description,
-      price,
-      image,
-      category,
-      rating,
-    } = props.products.product;
-    return (
-      <div>
-        <div>
-          <h1>{name}</h1>
-          <img src={image} />
-          <h3>Category: {category}</h3>
-        </div>
-        <div>
-          <p>{description}</p>
+  const { products } = props.products;
+  const paramId = Number(props.match.params.id);
+  const product = products.length
+    ? products.filter(product => {
+        return product.id === paramId;
+      })[0]
+    : [];
 
-          <div>
-            <h2>Price: {price}</h2>
-            <div>
-              <h2>Quanity:</h2>
-              <select>
-                <option>1</option>
-              </select>
-            </div>
-            <h2>Rating: {rating}</h2>
-          </div>
-          <button>Add To Cart</button>
-        </div>
+  return (
+    <div>
+      <div>
+        <h1>{product.title}</h1>
+        <img src={product.imageUrl} />
+        <h3>Category: Something</h3>
       </div>
-    );
-  }
+      <div>
+        <p>{product.description}</p>
+
+        <div>
+          <h2>Price: {product.price}</h2>
+          <div>
+            <h2>Quanity:</h2>
+            <select>
+              <option>1</option>
+            </select>
+          </div>
+          <h2>Rating: {product.rating}</h2>
+        </div>
+        <button>Add To Cart</button>
+      </div>
+    </div>
+  );
 };
 
 const mapToProps = state => {
-  console.log('MAPTOPROPS', state);
+  console.log("MAPTOPROPS", state);
   return {
-    products: state.products,
-    product: state.product,
+    products: state.products
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  console.log('MAPDISPATCH TO PROPS>>>>>', ownProps);
+  console.log("MAPDISPATCH TO PROPS>>>>>", ownProps);
   return {
     onLoad: function() {
       dispatch(fetchSingleProduct(ownProps.params.match.id));
-    },
+    }
   };
 };
 
@@ -71,10 +62,9 @@ const SingleProductContainer = connect(
 export default SingleProductContainer;
 
 SingleProduct.propTypes = {
-  product: PropTypes.object,
+  product: PropTypes.object
 };
 
 /**
  * PROP TYPES
  */
-
